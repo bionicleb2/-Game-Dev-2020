58d896a4187ddbda65a9961de5813ce6eba83db9
Brian Cox <brian.c120@stu.nebo.edu>
2019-09-24 10:32:24 -0600

    Added Plane Challenge

---
+++ Create With Code/Prototype 1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //transform.position = plane.transform.position + new Vector3(0, 0, -70);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
--- Create With Code/Prototype 1/Assets/Scripts/PlayerController.cs
+++ Create With Code/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20f;
-
+    private float speed = 20f;
+    private float turnSpeed = 45f;
+    private float horizontalInput;
+    private float forawrdInput;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +18,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per second
     void Update()
     {
-        //We'll move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        forawrdInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+        //We'll move the vehicle forward based on verticle input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forawrdInput);
+        //Turn the vehicle based on horizontal input
+        transform.Rotate(Vector3.up, horizontalInput * turnSpeed * Time.deltaTime);
     }
 }
