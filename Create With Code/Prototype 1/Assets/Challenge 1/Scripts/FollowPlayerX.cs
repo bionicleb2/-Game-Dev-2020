132970ba4c8562f401db314e42eec7fd2f86e42c
Brian Cox <brian.c120@stu.nebo.edu>
2019-09-26 09:43:59 -0600

    Update camera for Plane Challenge

--- Create With Code/Prototype 1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Create With Code/Prototype 1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    public Vector3 offset;
+    public Vector3 offset = new Vector3(30,0,10);
 
     // Start is called before the first frame update
     void Start()
     {
         //transform.position = plane.transform.position + new Vector3(0, 0, -70);
-
+        
     }
 
     // Update is called once per frame
