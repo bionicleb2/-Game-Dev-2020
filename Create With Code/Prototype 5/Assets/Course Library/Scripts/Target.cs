252052b6adaafe6aa823acf950ff7c8c055c9132
Brian Cox <brian.c120@stu.nebo.edu>
2019-12-16 10:08:57 -0700

    Created Prototype 5.

---
+++ Create With Code/Prototype 5/Assets/Course Library/Scripts/Target.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody targetRb;
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorque = 10;
+    private float xRange = 4;
+    private float ySpawnPos = -6;
+
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+        targetRb.AddForce(RandomForce(), FroceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+        transform.position = new Vector3(RandomSpawnPos);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    float RandomTorque()
+    {
+        return RandomForce.Range(-maxTorque, maxTorque);
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(RandomForce.Range(-xRange, xRange), ySpawnPos);
+    }
+}
